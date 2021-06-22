08c7ff5c3570e848b7724702260c50c19efbb563
naldal <cec7378@gmail.com>
2021-06-22 15:23:56 +0900

    Initial Commit

---
+++ CursedDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CursedDay
+//
+//  Created by 송하민 on 2021/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
